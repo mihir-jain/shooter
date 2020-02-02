c9c43c2829801de10fe388d208b0be951734cf1a
hp-programming <controlsystems@saintsrobotics.com>
2020-02-02 13:19:49 -0800

    The shooter with the bang bang loop

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -13,7 +13,9 @@ import com.revrobotics.CANSparkMaxLowLevel.MotorType;
 
 import edu.wpi.first.wpilibj.PWMVictorSPX;
 import edu.wpi.first.wpilibj.SpeedController;
+import edu.wpi.first.wpilibj.SpeedControllerGroup;
 import edu.wpi.first.wpilibj.TimedRobot;
+import edu.wpi.first.wpilibj.XboxController;
 import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 
@@ -34,10 +36,13 @@ public class Robot extends TimedRobot {
   private CANSparkMax RightShooter;
   private CANEncoder LeftEncoder;
   private CANEncoder RightEncoder;
+  private boolean isUptoSpeed;
+  private XboxController controller;
+  private SpeedControllerGroup shooter;
 
   /**
-   * This function is run when the robot is first started up and should be
-   * used for any initialization code.
+   * This function is run when the robot is first started up and should be used
+   * for any initialization code.
    */
   @Override
   public void robotInit() {
@@ -46,19 +51,25 @@ public class Robot extends TimedRobot {
     SmartDashboard.putData("Auto choices", m_chooser);
     m_kicker = new PWMVictorSPX(0);
     LeftShooter = new CANSparkMax(16, MotorType.kBrushless);
+    LeftShooter.setInverted(true);
     RightShooter = new CANSparkMax(17, MotorType.kBrushless);
     LeftEncoder = LeftShooter.getEncoder();
     RightEncoder = RightShooter.getEncoder();
+    isUptoSpeed = false;
+    controller = new XboxController(0);
+    shooter = new SpeedControllerGroup(LeftShooter, RightShooter);
+
 
   }
 
   /**
-   * This function is called every robot packet, no matter the mode. Use
-   * this for items like diagnostics that you want ran during disabled,
-   * autonomous, teleoperated and test.
+   * This function is called every robot packet, no matter the mode. Use this for
+   * items like diagnostics that you want ran during disabled, autonomous,
+   * teleoperated and test.
    *
-   * <p>This runs after the mode specific periodic functions, but before
-   * LiveWindow and SmartDashboard integrated updating.
+   * <p>
+   * This runs after the mode specific periodic functions, but before LiveWindow
+   * and SmartDashboard integrated updating.
    */
   @Override
   public void robotPeriodic() {
@@ -67,14 +78,15 @@ public class Robot extends TimedRobot {
 
   /**
    * This autonomous (along with the chooser code above) shows how to select
-   * between different autonomous modes using the dashboard. The sendable
-   * chooser code works with the Java SmartDashboard. If you prefer the
-   * LabVIEW Dashboard, remove all of the chooser code and uncomment the
-   * getString line to get the auto name from the text box below the Gyro
+   * between different autonomous modes using the dashboard. The sendable chooser
+   * code works with the Java SmartDashboard. If you prefer the LabVIEW Dashboard,
+   * remove all of the chooser code and uncomment the getString line to get the
+   * auto name from the text box below the Gyro
    *
-   * <p>You can add additional auto modes by adding additional comparisons to
-   * the switch structure below with additional strings. If using the
-   * SendableChooser make sure to add them to the chooser code above as well.
+   * <p>
+   * You can add additional auto modes by adding additional comparisons to the
+   * switch structure below with additional strings. If using the SendableChooser
+   * make sure to add them to the chooser code above as well.
    */
   @Override
   public void autonomousInit() {
@@ -89,13 +101,13 @@ public class Robot extends TimedRobot {
   @Override
   public void autonomousPeriodic() {
     switch (m_autoSelected) {
-      case kCustomAuto:
-        // Put custom auto code here
-        break;
-      case kDefaultAuto:
-      default:
-        // Put default auto code here
-        break;
+    case kCustomAuto:
+      // Put custom auto code here
+      break;
+    case kDefaultAuto:
+    default:
+      // Put default auto code here
+      break;
     }
   }
 
@@ -104,11 +116,42 @@ public class Robot extends TimedRobot {
    */
   @Override
   public void teleopPeriodic() {
-    m_kicker.set(1);
-    RightShooter.set(0.95);
-    LeftShooter.set(-0.95);
+    if (controller.getAButton()){
+      if (RightEncoder.getVelocity() > 5400){
+        isUptoSpeed = true;
+      } else{
+        isUptoSpeed = false;
+      }
+
+      if (isUptoSpeed){
+        if (controller.getBButton()){
+          m_kicker.set(1);
+        }
+        shooter.set(0);
+        
+      }
+      else{
+        m_kicker.set(0);
+        shooter.set(1);
+        
+      }
+    
+    } else if(controller.getXButton()){
+      shooter.set(0.45);
+      
+      m_kicker.set(1);
+    } else {
+      m_kicker.set(0);
+      shooter.set(0);
+    }
+  
+    
+    
     SmartDashboard.putNumber("left Shooter RPM", LeftEncoder.getVelocity());
     SmartDashboard.putNumber("Right Shooter RPM", RightEncoder.getVelocity());
+    SmartDashboard.putNumber("left Temp", LeftShooter.getMotorTemperature());
+    SmartDashboard.putNumber("right Temp", RightShooter.getMotorTemperature());
+
 
   }
 
